7169687bd1dabf42338c942c6140877608f43db0
reallywally <goldenale5500@gmail.com>
2025-03-27 23:52:41 +0900

    api 라우터랑 스타일링

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,7 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 // 모든 컴포넌트의 부모
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/test");
+  };
+
+  return (
+    <>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test로 이동</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
